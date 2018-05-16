ee13d418244ac66096bbdee94e2124d38ba45e3f
jasperjn <jaspjnn@gmail.com>
2018-05-16 16:23:52 +0800

    part one

---
+++ tutorial-part-one/src/pages/page-2.js
@@ -0,0 +1,11 @@
+import Link from 'gatsby-link'
+import React from 'react'
+
+
+
+export default () => (
+  <div>
+    <p>Hello world from my second Gatsby page</p>
+    <Link to="/">back home</Link>
+  </div>
+);
